bfbfc499e2ff20c9038aa69b6dd7bbcaac8f5cdb
prathamdubey <prathamd67@gmail.com>
2022-10-30 02:12:26 +0530

    logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <a href="https://www.tesla.com/">
+        <img src="./images/logo.svg" alt="logo" />
+      </a>
+      <Menu></Menu>
+    </Container>
+  );
 }
 
 export default Header;
+
+const Container = styled.div``;
+
+const Menu = styled.div``;
